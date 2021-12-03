af573ddfbaf8773dbcb4409c5e3710a487b67def
EmporerEmu <jjwilliams013@gmail.com>
2021-12-03 15:48:11 +0000

    Add a 'no match route'
    Added a 'no match' route using the special character '*'.
    This is a special use case that will only match when no routes match
    a URL.

--- src/index.jsx
+++ src/index.jsx
@@ -16,6 +16,15 @@ render(
         </Route>
         <Route path="itineraries" element={<Itineraries />} />
       </Route>
+      {/* The no match Route */}
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>Oops, looks like there is nothing here!</p>
+          </main>
+        }
+      />
     </Routes>
   </BrowserRouter>,
   rootElement
