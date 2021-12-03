25bf9ad8310ec97d1a0e631f94169b7d400cf2f2
EmporerEmu <jjwilliams013@gmail.com>
2021-12-03 17:18:36 +0000

    Wrap react-bootstrap links with 'LinkContainer'
    Using the library 'react-router-bootstrap' allows plain react bootstrap
    components to be wrapped and allow it to behave like a React Router
    <Link>.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,18 +2,23 @@ import { Link, Outlet } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "../../bootstrap_flatly.css";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Navbar>
+      <Navbar bg="dark" expand="md">
         <Container>
           <Navbar.Brand>NC 500</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navber-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Link to="/">Home</Link> | <Link to="/hostels">Hostels</Link> |{" "}
-              <Link to="/itineraries">Itineraries</Link>
+              <LinkContainer to="/hostels">
+                <Nav.Link>Hostels</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/itineraries">
+                <Nav.Link>Itineraries</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
