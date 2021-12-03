a41bd5cbbc0945af80d9cf9f903a33fdc52f3ce5
EmporerEmu <jjwilliams013@gmail.com>
2021-12-03 17:10:27 +0000

    Add basic Navbar and styling libraries
    Using 'react-bootstrap' and 'bootstrap@5', basic styling of the
    application has begun with the implementation of a navbar.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
-import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "../../bootstrap_flatly.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>NC 500</h1>
-      <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
-        <Link to="/hostels">Hostels</Link> |{" "}
-        <Link to="/itineraries">Itineraries</Link>
-      </nav>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>NC 500</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navber-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/">Home</Link> | <Link to="/hostels">Hostels</Link> |{" "}
+              <Link to="/itineraries">Itineraries</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Outlet />
     </div>
   );
