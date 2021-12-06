bcdfd7187b2dd8741944b153c754907e5b17c129
EmporerEmu <jjwilliams013@gmail.com>
2021-12-06 23:42:29 +0000

    Move Navbar into its own component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,30 +3,12 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "../../bootstrap_flatly.css";
 import { LinkContainer } from "react-router-bootstrap";
-
-console.log(process.env.REACT_APP_GOOGLE_STATIC_API_KEY);
+import Navigation from "./Navigation";
 
 function App() {
   return (
     <div>
-      <Navbar bg="dark" expand="md">
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>NC 500</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navber-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <LinkContainer to="/hostels">
-                <Nav.Link>Hostels</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/itineraries">
-                <Nav.Link>Itineraries</Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Navigation />
       <Outlet />
     </div>
   );
