a40fa38483f5ffc36e8cf047bccd11f4263ce8e1
EmporerEmu <jjwilliams013@gmail.com>
2021-12-11 22:02:23 +0000

    Add dynamic functionality to review component
    The reviews component now takes in a users input for a hostel review and
    sends it via POST request to the hosted backend. It uses the
    useParams() hook to get the hostelId from the URL and appends this to
    the API URL.

--- src/views/review/Review.jsx
+++ src/views/review/Review.jsx
@@ -3,29 +3,39 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 
 export default function Review() {
-  const [userInput, setUserInput] = useState([
-    {
-      reviewer: "",
-      review: "",
-    },
-  ]);
+  const [nameInput, setNameInput] = useState("");
+  const [reviewInput, setReviewInput] = useState("");
+  const params = useParams();
+  console.log("params", params.hostelId);
 
   const addReview = async () => {
     const query = {
-      reviewer: "jamie",
-      review: "test test",
+      reviewer: nameInput,
+      review: reviewInput,
     };
     console.log(query);
-    const res = await fetch("http://localhost:3000/hostels/review/1", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // mode: "no-cors",
-      body: JSON.stringify(query),
-    });
+    const res = await fetch(
+      "http://localhost:3000/hostels/review/" + params.hostelId,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // mode: "no-cors",
+        body: JSON.stringify(query),
+      }
+    );
     const data = await res.json();
     console.log(data);
+  };
+
+  const handleName = (e) => {
+    setNameInput(e.currentTarget.value);
+    console.log(nameInput);
+  };
+
+  const handleReview = (e) => {
+    setReviewInput(e.currentTarget.value);
   };
 
   return (
@@ -38,16 +48,21 @@ export default function Review() {
           <Form.Control
             type="text"
             placeholder="Your name here..."
+            onChange={handleName}
+            value={nameInput}
           ></Form.Control>
         </Form.Group>
         <Form.Group className="mb-3" controlId="reviewInput">
           <Form.Label>Review</Form.Label>
-          <Form.Control as="textarea" rows={4}></Form.Control>
+          <Form.Control
+            as="textarea"
+            rows={4}
+            onChange={handleReview}
+          ></Form.Control>
         </Form.Group>
         <Button
           variant="primary"
           onClick={() => {
-            //   navigate("/hostels");
             addReview();
           }}
         >
