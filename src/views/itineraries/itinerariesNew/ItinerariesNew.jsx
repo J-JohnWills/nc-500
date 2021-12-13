da08f581fabb702ba40fdf00426cb6249074744f
EmporerEmu <jjwilliams013@gmail.com>
2021-12-13 23:26:55 +0000

    Add select form inputs for starting hostel, date

--- src/views/itineraries/itinerariesNew/ItinerariesNew.jsx
+++ src/views/itineraries/itinerariesNew/ItinerariesNew.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import ItinerariesNewForm from "../itinerariesNewForm/ItinerariesNewForm";
 
 export default function ItinerariesNew({ hostelList }) {
   return (
     <div>
       <h1>New trip!</h1>
+      <ul>
+        <li>Create an itinerary for their trip</li>
+        <li>Setting a start date</li>
+        <li>Setting the number of stages</li>
+        <li>Each stage will specify a hostel and number of nights to stay</li>
+        <li>
+          Will be able to change or delete a specified stage of the itinerary
+        </li>
+        <li>
+          Should be able to view the itinerary, including the distance between
+          each stage and total distance to travel
+        </li>
+      </ul>
+      <ItinerariesNewForm hostelList={hostelList} />
     </div>
   );
 }
---
+++ src/views/itineraries/itinerariesNewForm/ItinerariesNewForm.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+
+export default function ItinerariesNewForm({ hostelList }) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [startingHostel, setStartingHostel] = useState("");
+  let navigate = useNavigate();
+
+  console.log("hostelList :", hostelList);
+
+  let selectItems = hostelList.map((hostel) => (
+    <option key={hostel.id}>{hostel.name}</option>
+  ));
+  console.log(selectItems);
+
+  const handleDateSelect = (e) => {
+    setStartDate(e.currentTarget.value);
+    console.log("startDate: ", startDate);
+  };
+
+  const handleHostelSelect = (e) => {
+    setStartingHostel(e.currentTarget.value);
+    console.log("starting hostel :", startingHostel);
+  };
+
+  function addStages() {
+    const starts = {
+      startDate: startDate,
+      startingHostel: startingHostel,
+    };
+  }
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formStartDate">
+        <Form.Label>Choose a start date</Form.Label>
+        <Form.Control
+          aria-label="Select start date"
+          type="date"
+          value={startDate.toUTCString}
+          onChange={handleDateSelect}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formStartLocation">
+        <Form.Label>Choose a starting hostel</Form.Label>
+        <Form.Select
+          aria-label="Select starting hostel"
+          value={startingHostel}
+          onChange={handleHostelSelect}
+        >
+          {selectItems}
+        </Form.Select>
+      </Form.Group>
+      <Button
+        variant="primary"
+        onClick={() => {
+          addStages();
+        }}
+      >
+        Add stages
+      </Button>
+    </Form>
+  );
+}
